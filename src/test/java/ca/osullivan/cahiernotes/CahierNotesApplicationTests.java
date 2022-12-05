3dd428cb98a68fa8c0f0760621ab061591e5c568
Fabrice Ilboudo <filboudo@nsidetech.ca>
2022-12-05 00:35:13 -0500

    Init project

---
+++ src/test/java/ca/osullivan/cahiernotes/CahierNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.osullivan.cahiernotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CahierNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
