620e26bdcb41403086be2737f0a92d4d27e66e1e
Fabrice Ilboudo <filboudo@nsidetech.ca>
2022-12-06 18:21:44 -0500

    Refactoring

--- src/main/java/ca/osullivan/cahiernotes/NoteController.java
+++ src/main/java/ca/osullivan/cahiernotes/NoteController.java
@@ -1,12 +1,14 @@
 package ca.osullivan.cahiernotes;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/notes")
+
 public class NoteController {
+    @PostMapping("/notes")
     public ResponseEntity<Object> saveNote() {
         //TODO
         return ResponseEntity.ok().build();
