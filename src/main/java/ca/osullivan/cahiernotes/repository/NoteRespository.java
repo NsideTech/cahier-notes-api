190a6406cc1bb9e5eed9d6bb162a1f3e2182f9b7
Lionel Mouatcho <lamouatcho@osullivan-quebec.qc.ca>
2022-12-07 17:54:24 -0500

    Retrait des changements de Jacky

--- src/main/java/ca/osullivan/cahiernotes/repository/NoteRespository.java
+++
@@ -1,9 +0,0 @@
-package ca.osullivan.cahiernotes.repository;
-
-import ca.osullivan.cahiernotes.entity.Note;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRespository extends CrudRepository<Note,Long> {
-}
